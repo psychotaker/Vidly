7b0d384ea242b41579b528522cbfd4f233f625ff
Teufik Cajlakovic <teufik97@gmail.com>
2019-03-07 00:37:05 +0100

    Added additional properties in Movie class

---
+++ Vidly/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public DateTime DateAdded { get; set; }
+        public int InStock { get; set; }
+
+    }
+
+    // /movies/random - creating controller
+}
